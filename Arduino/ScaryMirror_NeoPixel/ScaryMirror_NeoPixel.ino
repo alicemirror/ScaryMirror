463a343f42e733d1e3c8be719ba0e378e325482a
Enrico Miglino <balearicdynamics@gmail.com>
2018-10-20 04:05:59 +0200

    Added Arduino Sketches Folder

---
+++ Arduino/ScaryMirror_NeoPixel/ScaryMirror_NeoPixel.ino
@@ -0,0 +1,177 @@
+/**
+ * @file  ScaryMirror_NeoPixel
+ * @brief Neopixel functions on the Arduino. This version if for neopixel testing
+ * launching the lighting functions in sequence.
+ * 
+ * Incudes the time one interrupt to manage a 5 ms pulse every 5 sec
+ * to keep alive the smartphone battery pack (Arduino board and neopixel strip)
+ * 
+ * @author Enrico Miglino <balearicdynamics@gmail.com>
+ * @date Sep.-Oct. 2018
+ * 
+ */
+
+#include <Adafruit_NeoPixel.h>
+#include <TimerOne.h>
+
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 6
+
+// Pulse pin to avoid the power pack automatically shut down.
+#define PULSE_PIN 5
+// Uses the builtin LED to monitor the pulse generation to keep the power bank
+// battery always on
+#define PULSE_MONITOR LED_BUILTIN
+// Pulse duration. Should be empirically calibrated on the power bank used
+#define PULSE_MS 5
+// Interval between two pulses (long interval, about 1 second)
+#define PULSE_INTERVAL 5000000
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(144 , PIN, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+void setup() {
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+
+  pinMode(PULSE_PIN, OUTPUT);
+  pinMode(PULSE_MONITOR, OUTPUT);
+
+  digitalWrite(PULSE_PIN, HIGH);
+  digitalWrite(PULSE_MONITOR, LOW);
+
+  Timer1.initialize(PULSE_INTERVAL);
+  Timer1.attachInterrupt(pulsePower);
+  Timer1.start();
+}
+
+void loop() {
+  // Some example procedures showing how to display to the pixels:
+  colorWipe(strip.Color(255, 0, 0), 50); // Red
+  colorWipe(strip.Color(0, 255, 0), 50); // Green
+  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+//colorWipe(strip.Color(0, 0, 0, 255), 50); // White RGBW
+  // Send a theater pixel chase in...
+  theaterChase(strip.Color(127, 127, 127), 50); // White
+  theaterChase(strip.Color(127, 0, 0), 50); // Red
+  theaterChase(strip.Color(0, 0, 127), 50); // Blue
+
+  rainbow(20);
+  rainbowCycle(20);
+  theaterChaseRainbow(50);
+}
+
+// Timer callback function.
+// Stop the timer, output the pin for 20 ms then restart the timer
+void pulsePower(void)
+{
+  Timer1.stop();
+  digitalWrite(PULSE_PIN, LOW);  
+  digitalWrite(PULSE_MONITOR, LOW);
+  delay(PULSE_MS);
+  digitalWrite(PULSE_PIN, HIGH);
+  digitalWrite(PULSE_MONITOR, HIGH);
+  Timer1.start();
+}
+
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
